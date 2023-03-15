1e514690ed969991aaae00e3324bc7654f629573
Alejandro  Corona Esperón <100472181@alumnos.uc3m.es>
2023-03-16 00:46:02 +0100

    Clases de eq, vl y tests v1

--- src/main/python/uc3m_logistics/order_manager.py
+++ src/main/python/uc3m_logistics/order_manager.py
@@ -1,6 +1,6 @@
 """Module """
 from .order_request import OrderRequest
-
+from .order_management_exception import OrderManagementException
 
 class OrderManager:
     """Class for providing the methods for managing the orders"""
@@ -11,18 +11,45 @@ class OrderManager:
     def validate_ean13(ean13_code):
         """RETURNs TRUE IF THE CODE RECEIVED IS A VALID EAN13,
         OR FALSE IN OTHER CASE"""
+        #CE_NV_2
+        if not ean13_code.isdigit():
+            raise OrderManagementException("Invalid EAN13 code string")
+        #CE_NV_3
+        if not isinstance(ean13_code, str):
+            raise OrderManagementException("Invalid EAN13 code string")
+        if len(ean13_code) != 13:
+            raise OrderManagementException("Invalid EAN13 code string")
+        index = 0
+        result = 0
+        while index < 12:
+            if index % 2 == 0:
+                result += int(ean13_code[index])
+            else:
+                result += 3 * int(ean13_code[index])
+            index += 1
+        aux = result
+        while aux % 10 != 0:
+            aux += 1
+        aux -= result
+        if aux != int(ean13_code[index]):
+            raise OrderManagementException("Invalid EAN13 code string")
         return True
 
     @staticmethod
     def validate_order_type(ean13_code):
         """RETURNs TRUE IF THE CODE RECEIVED IS A VALID EAN13,
         OR FALSE IN OTHER CASE"""
+        if ean13_code.upper() != "REGULAR" and ean13_code.upper() != "PREMIUM":
+            raise OrderManagementException("Invalid order_type")
         return True
 
     @staticmethod
     def validate_address(ean13_code):
         """RETURNs TRUE IF THE CODE RECEIVED IS A VALID EAN13,
         OR FALSE IN OTHER CASE"""
+        a = len(ean13_code)
+        if len(ean13_code) < 20 or len(ean13_code) >= 100:
+            raise OrderManagementException("Invalid address length")
         return True
 
     @staticmethod
@@ -40,5 +67,8 @@ class OrderManager:
     @staticmethod
     def register_order (product_id, order_type, address, phone_number, zip_code):
         """Devuelve el id del producto"""
+        OrderManager.validate_ean13(product_id)
+        OrderManager.validate_order_type(order_type)
+        OrderManager.validate_address(address)
         my_order = OrderRequest(product_id, order_type, address, phone_number, zip_code)
         return my_order.order_id
--- src/unittest/python/test_register_order_tests.py
+++ src/unittest/python/test_register_order_tests.py
@@ -1,19 +1,74 @@
 """class for testing the regsiter_order method"""
 import unittest
 from uc3m_logistics import OrderManager
+from uc3m_logistics import OrderManagementException
 from freezegun import freeze_time
 
-
+@freeze_time("2023-03-08")
 class MyTestCase(unittest.TestCase):
     """class for testing the register_order method"""
-    @freeze_time("2023-03-08")
-    def test_ecv_1( self ):
+    def test_ecv_1(self):
         """dummy test"""
         my_manager = OrderManager()
         my_manager_id = my_manager.register_order\
             ("8421691423220", "REGULAR", "C/LISBOA, 4,MADRID, SPAIN", "+34123456789", "28005")
         self.assertEqual("8e290b2ebc51e1634f124c9f1151f88c", my_manager_id)
-        #comprobar el fichero
+
+    def test_ecv_2(self):
+        """Product ID not a number"""
+        my_manager = OrderManager()
+        with self.assertRaises(OrderManagementException) as cm:
+            my_manager.register_order\
+                ("842169142322A", "PREMIUM", "C/LISBOA,4, MADRID, SPAIN", "+34123456789", "28005")
+        self.assertEqual("Invalid EAN13 code string", cm.exception.message)
+
+    def test_ecv_3(self):
+        """Product ID not check sum"""
+        my_manager = OrderManager()
+        with self.assertRaises(OrderManagementException) as cm:
+            my_manager.register_order\
+                ("8421691423225", "REGULAR", "C/LISBOA,4, MADRID, SPAIN", "+34123456789", "28005")
+        self.assertEqual("Invalid EAN13 code string", cm.exception.message)
+
+    def test_ecv_8(self):
+        """Order_type wrong"""
+        my_manager = OrderManager()
+        with self.assertRaises(OrderManagementException) as cm:
+            my_manager.register_order \
+                ("8421691423220", "PRE", "C/LISBOA,4, MADRID, SPAIN", "+34123456789", "28005")
+        self.assertEqual("Invalid order_type", cm.exception.message)
+
+    def test_ecv_18(self):
+        """19 characters for address, cota inferior invalida"""
+        my_manager = OrderManager()
+        with self.assertRaises(OrderManagementException) as cm:
+            my_manager.register_order \
+                ("8421691423220", "PREMIUM", "C/LIS,4, MADR, SPAI", "+34123456789", "28005")
+        self.assertEqual("Invalid address length", cm.exception.message)
+
+    def test_ecv_17(self):
+        """20 characters for address, cota inferior valida"""
+        my_manager = OrderManager()
+        my_manager_id = my_manager.register_order \
+            ("8421691423220", "PREMIUM", "C/LIS,4, MADR, SPAIN", "+34123456789", "28005")
+        self.assertEqual("9a6397e11aed67e554fcf245af4eb43c", my_manager_id)
+
+    def test_ecv_19(self):
+        """99 characters for address, cota superior valida"""
+        my_manager = OrderManager()
+        my_manager_id = my_manager.register_order \
+            ("8421691423220", "PREMIUM", "C/LISBOA LA MEJOR CIUDAD DEL MUND0,12345678999, "
+            "MADRID DEMASIADA CONTAMINACION, SPAIN PERO SIN LA S", "+34123456789", "28005")
+        self.assertEqual("3eb1a4fb8c37ffc300fae92c0714e48c", my_manager_id)
+
+    def test_ecv_20(self):
+        """100 characters for address, cota superior invalida"""
+        my_manager = OrderManager()
+        with self.assertRaises(OrderManagementException) as cm:
+            my_manager.register_order \
+                ("8421691423220", "PREMIUM", "C/LISBOA LA MEJOR CIUDAD DEL MUND0,123456789999, "
+                "MADRID DEMASIADA CONTAMINACION, SPAIN PERO SIN LA S", "+34123456789", "28005")
+        self.assertEqual("Invalid address length", cm.exception.message)
 
 
 if __name__ == '__main__':
