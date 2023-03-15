808527e8476cdd5058ee85e234d17699e0341867
kaipo008 <100472080@alumnos.uc3m.es>
2023-03-15 22:43:50 +0100

    hola

--- src/main/python/uc3m_logistics/order_manager.py
+++ src/main/python/uc3m_logistics/order_manager.py
@@ -41,4 +41,5 @@ class OrderManager:
     def register_order (product_id, order_type, address, phone_number, zip_code):
         """Devuelve el id del producto"""
         my_order = OrderRequest(product_id, order_type, address, phone_number, zip_code)
+        print(my_order)
         return my_order.order_id
--- src/unittest/python/test_register_order_tests.py
+++ src/unittest/python/test_register_order_tests.py
@@ -4,9 +4,10 @@ from uc3m_logistics import OrderManager
 from freezegun import freeze_time
 
 
+@freeze_time("2023-03-08")
 class MyTestCase(unittest.TestCase):
     """class for testing the register_order method"""
-    @freeze_time("2023-03-08")
+
     def test_ecv_1( self ):
         """dummy test"""
         my_manager = OrderManager()
@@ -14,7 +15,6 @@ class MyTestCase(unittest.TestCase):
             ("8421691423220", "REGULAR", "C/LISBOA, 4,MADRID, SPAIN", "+34123456789", "28005")
         self.assertEqual("8e290b2ebc51e1634f124c9f1151f88c", my_manager_id)
         #comprobar el fichero
-
 
 if __name__ == '__main__':
     unittest.main()
