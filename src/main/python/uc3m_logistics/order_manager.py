3a4a30369f2575b268aa7b942dd4460ae936feb9
kaipo008 <100472080@alumnos.uc3m.es>
2023-03-23 15:09:48 +0100

    hola

--- src/main/python/uc3m_logistics/order_manager.py
+++ src/main/python/uc3m_logistics/order_manager.py
@@ -2,6 +2,7 @@
 from .order_request import OrderRequest
 from .order_management_exception import OrderManagementException
 
+
 class OrderManager:
     """Class for providing the methods for managing the orders"""
     def __init__(self):
@@ -9,8 +10,8 @@ class OrderManager:
 
     @staticmethod
     def validate_ean13(ean13_code):
-        """RETURNs TRUE IF THE CODE RECEIVED IS A VALID EAN13,
-        OR FALSE IN OTHER CASE"""
+        #RETURNs TRUE IF THE CODE RECEIVED IS A VALID EAN13,
+        #OR FALSE IN OTHER CASE
         #CE_NV_2
         if not ean13_code.isdigit():
             raise OrderManagementException("Invalid EAN13 code string")
@@ -47,7 +48,6 @@ class OrderManager:
     def validate_address(ean13_code):
         """RETURNs TRUE IF THE CODE RECEIVED IS A VALID EAN13,
         OR FALSE IN OTHER CASE"""
-        a = len(ean13_code)
         if len(ean13_code) < 20 or len(ean13_code) >= 100:
             raise OrderManagementException("Invalid address length")
         return True
@@ -84,7 +84,7 @@ class OrderManager:
         return True
 
     @staticmethod
-    def register_order (product_id, order_type, address, phone_number, zip_code):
+    def register_order(product_id, order_type, address, phone_number, zip_code):
         """Devuelve el id del producto"""
         OrderManager.validate_ean13(product_id)
         OrderManager.validate_order_type(order_type)
