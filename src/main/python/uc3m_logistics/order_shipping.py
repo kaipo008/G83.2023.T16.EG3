1b10ec74f286487c0ce6c9b0760074e4bb07f778
kaipo008 <100472080@alumnos.uc3m.es>
2023-03-26 17:55:08 +0200

    hola

--- src/main/python/uc3m_logistics/order_shipping.py
+++ src/main/python/uc3m_logistics/order_shipping.py
@@ -2,7 +2,8 @@
 from datetime import datetime
 import hashlib
 
-class OrderShipping():
+
+class OrderShipping:
     """Class representing the information required for shipping of an order"""
 
     def __init__(self, product_id, order_id, delivery_email, order_type):
@@ -18,31 +19,32 @@ class OrderShipping():
         else:
             delivery_days = 1
 
-        #timestamp is represneted in seconds.microseconds
-        #__delivery_day must be expressed in senconds to be added to the timestap
+        """timestamp is represneted in seconds.microseconds
+        __delivery_day must be expressed in senconds to be added to the timestap"""
         self.__delivery_day = self.__issued_at + (delivery_days * 24 * 60 * 60)
 
     def __signature_string(self):
         """Composes the string to be used for generating the key for the date"""
-        return "{alg:" + self.__alg +",typ:" + self.__type +",order_id:" + \
+        return "{alg:" + self.__alg + ",typ:" + self.__type + ",order_id:" + \
                self.__order_id + ",issuedate:" + str(self.__issued_at) + \
                ",deliveryday:" + str(self.__delivery_day) + "}"
 
     @property
-    def product_id( self ):
+    def product_id(self):
         """Property that represents the product_id of the patient"""
         return self.__product_id
 
     @product_id.setter
-    def product_id( self, value ):
+    def product_id(self, value):
         self.__product_id = value
 
     @property
     def order_id( self ):
         """Property that represents the order_id"""
         return self.__order_id
+
     @order_id.setter
-    def order_id( self, value ):
+    def order_id(self, value):
         self.__order_id = value
 
     @property
@@ -55,7 +57,7 @@ class OrderShipping():
         self.__delivery_email = value
 
     @property
-    def tracking_code( self ):
+    def tracking_code(self):
         """Returns the sha256 signature of the date"""
         return hashlib.sha256(self.__signature_string().encode()).hexdigest()
 
@@ -65,10 +67,10 @@ class OrderShipping():
         return self.__issued_at
 
     @issued_at.setter
-    def issued_at( self, value ):
+    def issued_at(self, value):
         self.__issued_at = value
 
     @property
-    def delivery_day( self ):
+    def delivery_day(self):
         """Returns the delivery day for the order"""
         return self.__delivery_day
